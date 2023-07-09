e93ba1026c32d883b0853407027ca10a73000cbd
itsayushk19 <ltsaayushk193@gmail.com>
2023-07-09 21:32:47 +0530

    Latest Version

--- pages/tools/[category]/index.js
+++ pages/tools/[category]/index.js
@@ -1,17 +1,41 @@
 import ArchieveLayout from "components/layouts/Archieve";
 import { getAllToolsCategorized, getCategory } from "/utils/tools";
 
-export async function Archieve() {
-  const category = `coding`
-  const categorizedTools = await getAllToolsCategorized()
-  const categoryObject = await getCategory(category) 
+export default function Archieve({ categorizedTools, category, categoryObject }) {
   return (
     <>
       <ArchieveLayout
         categorizedTools={categorizedTools}
         category={category}
-        categoryObject = {categoryObject}
+        categoryObject={categoryObject}
       ></ArchieveLayout>
     </>
   );
+}
+
+export async function getStaticProps({ params }) {
+  const category = params.category;
+  const categorizedTools = await getAllToolsCategorized();
+  const categoryObject = await getCategory(category);
+
+  return {
+    props: {
+      categorizedTools,
+      category,
+      categoryObject,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const categorizedTools = await getAllToolsCategorized();
+
+  const paths = Object.keys(categorizedTools).map((category) => ({
+    params: { category },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
